3a280bd1578a0a3faa9b6f5cac3f4ed74414d9c5
theinfamousben <benjamin.mueller@bsstudios.org>
2026-01-24 12:40:45 +0100

    fix: add missing .js extension and imports in post-command.js

--- helpers/post-command.js
+++ helpers/post-command.js
@@ -1,16 +1,21 @@
-import { loadSettings } from "./config";
+import chalk from 'chalk';
+import { loadSettings } from "./config.js";
+import { fetchJson } from "./utils.js";
+import version from "./getv.js";
 
 async function checkForUpdates() {
-    const packageInfo = await fetchJson('https://registry.npmjs.org/@bobschlowinskii/clicraft');
-    const latestVersion = packageInfo['dist-tags'].latest;
-    const currentVersion = version;
+    try {
+        const packageInfo = await fetchJson('https://registry.npmjs.org/@bobschlowinskii/clicraft');
+        const latestVersion = packageInfo['dist-tags'].latest;
+        const currentVersion = version;
 
-    if (latestVersion !== currentVersion) {
-        console.log(chalk.yellow(`\n✓ A new CLIcraft version is available (v${latestVersion})`));
-        console.log(chalk.gray('  Run ') + chalk.cyan('npm install -g @bobschlowinskii/clicraft') + chalk.gray(' to update.\n'));
-        return;
+        if (latestVersion !== currentVersion) {
+            console.log(chalk.yellow(`\n✓ A new CLIcraft version is available (v${latestVersion})`));
+            console.log(chalk.gray('  Run ') + chalk.cyan('npm install -g @bobschlowinskii/clicraft') + chalk.gray(' to update.\n'));
+        }
+    } catch (error) {
+        // Silently ignore update check failures
     }
-
 }
 
 export function callPostCommandActions() {
